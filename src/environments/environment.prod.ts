78892e1e3e593b00d6df2635b0e79e10ca0d34de
Vasyl Halkovych <vasyl_halkovych@epam.com>
2021-08-28 19:30:56 +0300

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCHONvlD9hVAQccMzUQJRVsWQ-MdzFkeq4",
+    authDomain: "ministeamspa.firebaseapp.com",
+    projectId: "ministeamspa",
+    storageBucket: "ministeamspa.appspot.com",
+    messagingSenderId: "292489598562",
+    appId: "1:292489598562:web:e2ca1ee3bfb38819e6a33b"
+  }
 };
